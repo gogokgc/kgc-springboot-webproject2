20b4456b50c09ea35bf4b2974255b3945802558f
gogokgc <gogokgc@gmail.com>
2021-05-26 22:25:26 +0900

    Add Edit posting

--- kgc-springboot-webproject2/src/main/resources/static/js/index.js
+++ kgc-springboot-webproject2/src/main/resources/static/js/index.js
@@ -1,28 +1,54 @@
 var main = {
-	init: function(){
+	init: function() {
 		var _this = this;
-		$('#btn-save').on('click', function(){
+		$('#btn-save').on('click', function() {
 			_this.save();
 		});
+
+		$('#btn-update').on('click', function() {
+			_this.update();
+		});
 	},
-	
-	save: function(){
+
+	save: function() {
 		var data = {
 			title: $('#title').val(),
 			content: $('#content').val(),
 			author: $('#author').val()
 		};
-		
+
 		$.ajax({
 			type: 'POST',
 			url: '/api/v1/posts',
 			dataType: 'json',
 			contentType: 'application/json; charset=utf-8',
 			data: JSON.stringify(data)
-		}).done(function(){
+		}).done(function() {
 			alert('Posting Complete');
 			window.location.href = '/';
-		}).fail(function(error){
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		})
+	},
+
+	update: function() {
+		var data = {
+			title: $('#title').val(),
+			content: $('#content').val()
+		};
+
+		var id = $('#id').val();
+
+		$.ajax({
+			type: 'PUT',
+			url: '/api/v1/posts/' + id,
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8',
+			data: JSON.stringify(data)
+		}).done(function() {
+			alert('Editing Complete');
+			window.location.href = '/';
+		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		})
 	}
